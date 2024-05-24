ca74eb4e68ec02c7309820593530d7b096a7f926
AdamDunmore <adamfdunmore@gmail.com>
2024-05-24 21:59:00 +0100

    Re-added home.nix (whoops)

---
+++ nixos/modules/home.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, pkgs-unstable, hyprland, split-monitor-workspaces, spicetify-nix, inputs, ... }:
+
+let
+in
+{
+  imports = [
+    (import ./configs/hyprland.nix { inherit pkgs; inherit hyprland; inherit inputs;}) 
+    ./configs/kitty.nix
+    ./configs/zsh.nix
+    (import ./configs/spicetify.nix { inherit pkgs; inherit spicetify-nix; })
+  ];
+
+  home.username = "adam";
+  home.homeDirectory = "/home/adam";
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+
+  home.packages = (with pkgs; [
+    #Stable Packages
+    spicetify-cli
+    scanmem
+    brave
+    firefox
+    vscode
+    logseq
+    lutris
+    discord
+    vesktop
+    heroic
+    prismlauncher
+    motrix
+    android-studio
+  ])
+
+  ++
+
+  (with pkgs-unstable; [
+    #Unstable Packages
+    zed-editor
+    neovim
+  ]);
+}
+
