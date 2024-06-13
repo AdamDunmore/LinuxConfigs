560146ae9ddf5c74494bbbe2ef68d93f1db9a5ec
AdamDunmore <adamfdunmore@gmail.com>
2024-06-13 15:15:24 +0100

    Added wordwrap to track name

--- .config/ags/modules/playing.js
+++ .config/ags/modules/playing.js
@@ -3,12 +3,19 @@ const player = await Service.import('mpris')
 player.connect('changed', function(s){
     now_playing_title.set_label(s.players[0].track_title)
     now_playing_artist.set_label(s.players[0].track_artists[0])
-    now_playing_cover.setCss(`background-image: url('${s.players[0].track_cover_url}')`)
+    if (s.players[0].track_cover_url == ""){
+        now_playing_cover.hide()
+    }
+    else{
+        now_playing_cover.show()
+        now_playing_cover.setCss(`background-image: url('${s.players[0].track_cover_url}')`)
+    }
 })
 
 const now_playing_title = Widget.Label({
         label: "Nothings playing",
         className: "now_playing_info_title",
+        wrap: true
 })
 
 
