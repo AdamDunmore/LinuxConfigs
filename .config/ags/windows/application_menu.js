dc01e32eb1203e80bbc9086c0cbfe8d93aaaf542
AdamDunmore <adamfdunmore@gmail.com>
2024-06-14 12:14:33 +0100

    Finished Application Menu

--- .config/ags/windows/application_menu.js
+++ .config/ags/windows/application_menu.js
@@ -1,6 +1,8 @@
 // Code heavily inspired by https://github.com/Aylur/ags/blob/main/example/applauncher/applauncher.js
 const applications = await Service.import("applications")
 
+let active = 0;
+
 const App_Entry = app => Widget.Button({
     child: Widget.Box({
         spacing: 10,
@@ -31,13 +33,6 @@ const App_Input = Widget.Entry({
     on_change: ({ text }) => apps.forEach(item => {
         item.visible = item.attribute.app.match(text ?? "")
     }),
-    on_accept: (self) => {
-        const results = apps.filter((item) => item.visible);
-        if (results[0]){
-            close()
-            results[0].attribute.app.launch()
-        }
-    },
 })
 
 const App_Launcher = Widget.Window({
@@ -64,14 +59,41 @@ const App_Launcher = Widget.Window({
 
 App_Launcher.connect("key_press_event", (s, t) => {
     let keyval = t.get_keyval()[1];
+
     if (keyval == 65307){ //If key is escape then close
         close()
     }
+    else if (keyval == 65362){ //Up arrow key
+       setActive("-") 
+    } 
+    else if (keyval == 65364){ //Down Arrow Key
+        setActive("+")
+    }
+    else if (keyval == 65293){ //Enter key
+        const results = apps.filter((item) => item.visible);
+        results[active].attribute.app.launch();
+        close()
+    } 
 })
 
 function close(){
     App_Launcher.hide()
     App_Input.set_text("")
 }
+
+function setActive(state){
+    const results = apps.filter((item) => item.visible);
+    results[active].class_name = "app_menu_entry"    
+    if (state == "+"){
+        active += 1
+    }
+    else if (state == "-"){
+        active -= 1
+    }
+    if(active < 0){
+        active = 0
+    }
+    results[active].class_name = "app_menu_entry active"
+}setActive()
 
 export default App_Launcher
