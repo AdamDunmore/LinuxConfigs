cc0c43ec9011f5d9d1662d991ce9ab34c1d54e4b
AdamDunmore <adamfdunmore@gmail.com>
2024-07-01 12:24:08 +0100

    Removed Zellij

--- nixos/modules/configs/zellij.nix
+++ nixos/modules/configs/zellij.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, ... }:
 
 {
-    programs.zellij = {
-        enable = true;
-        enableZshIntegration = true;
-        settings = {
-            default_shell = "zsh";
-            theme = "nord";
-        };
-    };
+    # programs.zellij = {
+    #     enable = true;
+    #     enableZshIntegration = true;
+    #     settings = {
+    #         default_shell = "zsh";
+    #         theme = "nord";
+    #     };
+    # };
 }
