b7b165caa1e95cbd3796b0083323054cb1036832
AdamDunmore <adamfdunmore@gmail.com>
2024-08-07 09:26:00 +0100

    Fixed volume and brightness control

--- nixos/modules/configs/sway.nix
+++ nixos/modules/configs/sway.nix
@@ -91,9 +91,9 @@ in
                 "${mod}+Shift+s" = "move scratchpad";
                 "${mod}+s" = "scratchpad show";
 
-                "XF86AudioRaiseVolume" = "exec patctl set-sink-volume @DEFAULT_SINK@ +5%";
-                "XF86AudioLowerVolume" = "exec patctl set-sink-volume @DEFAULT_SINK@ -5%";
-                "XF86AudioMute" = "exec patctl set-sink-volume @DEFAULT_SINK@ 0%";
+                "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
+                "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+                "XF86AudioMute" = "exec pactl set-sink-volume @DEFAULT_SINK@ 0%";
                 "XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
                 "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
             };
