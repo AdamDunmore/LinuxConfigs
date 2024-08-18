22c144dbeb4de31a3148c0391b89fb6e7b7a03c0
AdamDunmore <adamfdunmore@gmail.com>
2024-08-18 19:03:29 +0100

    Commented out splash image due to it being useless currently

--- nixos/modules/system/bootloader.nix
+++ nixos/modules/system/bootloader.nix
@@ -1,15 +1,15 @@
 {
     boot.loader = {
-        efi.canTouchEfiVariables = true; 	
+        efi.canTouchEfiVariables = true;
+        timeout = 3;
         grub = {
             enable = true;
             backgroundColor = "#2E3440";
-            timeout = 3;
             device = "nodev";
             useOSProber = true;
             efiInstallAsRemovable = false;
             efiSupport = true;
-            splashImage = ../../../wallpapers/wallpaper_nixos.png;
+            # splashImage = ../../../wallpapers/wallpaper_nixos.png;
         };
         systemd-boot.enable = false;   
     };
