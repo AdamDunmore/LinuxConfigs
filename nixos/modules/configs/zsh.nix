fbeb9f27df15a52f28b3849b5be458857536fd3c
AdamDunmore <adamfdunmore@gmail.com>
2024-08-23 12:49:08 +0100

    Removed Oh-my-zsh

--- nixos/modules/configs/zsh.nix
+++ nixos/modules/configs/zsh.nix
@@ -12,10 +12,6 @@ in
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
         enableCompletion = true;
-        oh-my-zsh = {
-            enable = true;
-            theme = "agnoster";
-        };
         envExtra = "
         HOSTNAME=$(hostname)
         ";
