3ba188ac2650b9371a6dcd11ea3c56aed7f2be35
AdamDunmore <adamfdunmore@gmail.com>
2024-08-23 01:26:30 +0100

    Made starship look nice

--- nixos/modules/configs/starship.nix
+++ nixos/modules/configs/starship.nix
@@ -3,7 +3,17 @@
         enable = true;
         enableZshIntegration = true;
         settings = {
-            format = "(bold green)$directory(bold green)";
+            format = "[ ](#3B4252)$username[](fg:#3B4252 bg:#4C566A)$directory[ ](#4C566A)";
+            username = {
+                style_user = "bg:#3B4252 fg:#D8DEE9";
+                disabled = false;
+                show_always = true;
+                format = "[$user ]($style)";
+            };
+            directory = {
+                format = "[ $path ]($style)";
+                style = "bg:#4C566A fg:#D8DEE9";
+            };
         };
     };
 }
