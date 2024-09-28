f0315333250908b45327897bb1fe761640d2eeb1
AdamDunmore <adamfdunmore@gmail.com>
2024-09-28 13:49:45 +0100

    .

--- nixos/modules/main.nix
+++ nixos/modules/main.nix
@@ -26,48 +26,4 @@ in
 
     system.stateVersion = "${core.version}";	
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
-<<<<<<< HEAD
-
-    # Downloading Nerd Font
-    fonts.packages = with pkgs; [
-	    (nerdfonts.override { fonts = [ "CodeNewRoman" ]; })
-    ];
-
-    #Enabling Programs
-    programs = {
-        steam.enable = true;
-        river.enable = true;
-    };
-
-    # Services (Mainly for AGS)
-    services = {
-        libinput.enable = true;
-        printing.enable = true;
-        power-profiles-daemon.enable = true;
-        gvfs.enable = true;
-    };
-
-    # Enable networking
-    networking.networkmanager.enable = true;
-
-    # Enable Waydroid
-    virtualisation.waydroid.enable = true;
-
-    # Dotnet location fix
-    environment.sessionVariables = {
-      DOTNET_ROOT = "${pkgs.dotnet-sdk_7}";
-    };
-
-    # Enables Flatpak
-    services.flatpak.enable = true;
-
-    #Enabled Dynamically Linked Executables
-    programs.nix-ld = {
-        enable = true;
-        libraries =  with pkgs; [
-
-        ];
-    };
-=======
->>>>>>> 7f3d26a873b0a3b6e540306bf5bd5456b93092b5
 }
