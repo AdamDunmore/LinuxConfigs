fdb17ddc9a2287a7dbfacb6207144b24bf79da34
AdamDunmore <adamfdunmore@gmail.com>
2025-01-27 11:56:36 +0000

    Changed create_proj script to be easier to expand

--- nixos/modules/home/scripts/scripts/create_proj.sh
+++ nixos/modules/home/scripts/scripts/create_proj.sh
@@ -1,25 +1,18 @@
 #!/bin/sh
 
 # A script to build a project given an argument for a languge
-
-python() {
-    mkdir ./src
-    echo 'print("Hello World!")' >> ./src/main.py
-    printf '#!/bin/sh\npython3 ./src/main.py' >> build.sh
-    chmod +x ./build.sh
-}
-
 if [ "$1" = "" ]; then # Could maybe move to an input call
     echo "Incorrect Usage. Correct usage:"
     echo "create_proj <language>"
     exit
 fi
 
+langs=(
+    "python"
+)
 
-case "$1" in
-    "python")
-        python;;
-    *)
-        echo "Language not found or not supported"
-        ;;
-esac
+script_path="$(dirname $0)"
+
+if [[ ${langs[*]} =~ $1 ]]; then
+    cp "$script_path"/create_proj/templates/"$1"/. "$PWD" -r
+fi
