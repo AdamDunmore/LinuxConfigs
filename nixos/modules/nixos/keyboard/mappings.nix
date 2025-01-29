fcd44dad3dc0a3a6943dc8308a3ae7781b3fc3d7
Adam Dunmore <adamfdunmore@gmail.com>
2025-01-29 10:37:21 +0000

    [Nixos] added keybind to enable caps lock if i need it

--- nixos/modules/nixos/keyboard/mappings.nix
+++ nixos/modules/nixos/keyboard/mappings.nix
@@ -16,6 +16,7 @@ with lib;
                     esc = "`"; 
                     z = "102nd";
                     #p = "marco(G-102nd space)";
+                    c = "capslock";
                 };
             };
         };   
