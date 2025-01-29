6f195516dd12e548253254fe129621f6da332999
Adam Dunmore <adamfdunmore@gmail.com>
2025-01-29 15:12:32 +0000

    [Nixos] Begun adding xbox controller remappings

--- nixos/modules/nixos/keyboard/mappings.nix
+++ nixos/modules/nixos/keyboard/mappings.nix
@@ -6,12 +6,34 @@ in
 with lib;
 {
     config = mkIf cfg.custom_layout {
+        # Too fix
+        # hardware.uinput.enable = true;
+        # services.evdevremapkeys = {
+        #     enable = true;
+        #     settings = {
+        #         devices = [
+        #             { 
+        #                 input_name = "EasySMX EasySMX X10";
+        #                 output_name = "remap-controller";
+        #                 remappings = {
+        #                     BTN_SOUTH = [ "KEY_Z" ];
+        #                 };
+        #             }
+        #                 # input = "/dev/input/event6";
+        #                 # grab = true;
+        #                 # remappings = [
+        #                 #     { from = "BTN_SOUTH"; to = "KEY_S"; }
+        #                 # ];
+        #         ];
+        #     };
+        # };
         services.keyd = {
             enable = true;
             keyboards.default = {
                 settings = {
                     main = {
                         capslock = "layer(renamed)";
+
                     };
                     renamed = {
                         esc = "`"; 
